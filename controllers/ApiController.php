b1c11beb47c31a1933b28ba92685729ed2684e44
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-08-05 10:49:58 +0200

    fix(headers in file): add missing

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the YesWiki Extension Shop.
+ *
+ * Authors : see README.md file that was distributed with this source code.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace YesWiki\Shop\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
