98a7dab3e0653fe344bc00e8e367299d824577a7
Eliot Boutet <bouteteliot@gmail.com>
2023-07-17 19:26:01 +0200

    Add create default form for handling when the param is empty and the update config function

--- handlers/UpdateHandler__.php
+++ handlers/UpdateHandler__.php
@@ -9,8 +9,6 @@ use YesWiki\Security\Controller\SecurityController;
 
 class UpdateHandler__ extends YesWikiHandler
 {
-    private $formsIds;
-
     private const PATHS = [
         'forms' => [
             'Produit' => 'tools/shop/setup/forms/Form - Produit.txt',
@@ -19,72 +17,100 @@ class UpdateHandler__ extends YesWikiHandler
 
     public function run()
     {
+        $formManager = $this->getService(FormManager::class);
         if ($this->getService(SecurityController::class)->isWikiHibernated()) {
             throw new \Exception(_t('WIKI_IN_HIBERNATION'));
         };
         if (!$this->wiki->UserIsAdmin()) {
             return null;
         }
-        $this->formsIds = [];
-        foreach (self::PATHS['forms'] as $formName => $filePath) {
-            $this->formsIds[$formName] = $this->params->get('shop')['forms'][$formName.'_id'] ?? null;
+        dump($formManager->getAll());
+
+        $formIdsParam = $this->params->get('shop')['forms']['products'] ?? '';
+        dump($formIdsParam);
+
+        if ($formIdsParam == '') {
+            $this->createDefaultForm();
+            return;
         }
 
-        foreach ($this->formsIds as $formId => $formName) {
-            $this->getFormOrCreate($formName);
+        $productsFormIds = array_filter(array_map('trim', explode(',', $formIdsParam)));
+        foreach ($productsFormIds as $productsFormId) {
+            $this->getFormOrCreate($productsFormId);
         }
 
         return null;
     }
 
     /**
-     * @param string $formName
+     * @param string $formId
      * @return null|string $formId
      */
-    private function getFormOrCreate(string $formName): ?string
+    private function getFormOrCreate(string $formId)
     {
         // get services
         $formManager = $this->getService(FormManager::class);
-        // get Form Id
-        $formId = $this->formsIds[$formName] ;
-
         // get Form
         if (!empty($formId) && intval($formId) === intval(strval($formId))) {
             $form = $formManager->getOne($formId);
+            if (!$form) {
+                return;
+            }
+            $this->createDefaultForm();
+        }
+        return null;
+    }
+
+    private function createDefaultForm(): void
+    {
+        $formManager = $this->getService(FormManager::class);
+
+        if (!file_exists(self::PATHS['forms']['Produit'])) {
+            $this->render('@templates/alert-message.twig', [
+                'type' => 'danger',
+                'message' => str_replace(
+                    ['{formName}', '{filePath}'],
+                    ['Produit', self::PATHS['forms']['Produit']],
+                    _t('BENEVOLAT_UPDATE_FORM_ERROR')
+                ),
+            ]);
+            return;
         }
 
-        //Change this code
-        if (empty($form['bn_label_nature'])) {
-            // create a form
-            $formId = $formManager->findNewId();
-            if (!$formManager->create([
-                'bn_label_nature' => $formName,
-                'bn_template'=> 'texte***bf_titre***Titre de la fiche*** *** *** *** ***text***1*** *** *** * *** * *** *** *** ***',
-                'bn_description'=> '',
-                'bn_sem_context'=> '',
-                'bn_sem_type'=> '',
-                'bn_condition'=> '',
-                'bn_id_nature' => null,
-            ])) {
-                return null;
-            };
-        }
-        // save id in wakka.config.php
+        $formTemplate = file_get_contents(self::PATHS['forms']['Produit']);
+        $formId = $formManager->findNewId();
+        if (!$formManager->create([
+            'bn_label_nature' => 'Produit',
+            'bn_template' => $formTemplate,
+            'bn_description' => '',
+            'bn_sem_context' => '',
+            'bn_sem_type' => '',
+            'bn_condition' => '',
+            'bn_id_nature' => $formId,
+        ])) {
+            return;
+        };
+
+        $this->updateWakkaConfig($formId);
+    }
+
+    private function updateWakkaConfig($formId)
+    {
         if (!empty($formId)) {
             include_once 'tools/templates/libs/Configuration.php';
             $config = new Configuration('wakka.config.php');
             $config->load();
 
-            $baseKey = 'benevolat';
+            $baseKey = 'shop';
             $tmp = isset($config->$baseKey) ? $config->$baseKey : [];
-            if (!isset($tmp['forms'])) {
-                $tmp['forms'] = [];
+            if (!isset($tmp['forms']['products'])) {
+                $tmp['forms'] = [
+                    'products' => strval($formId)
+                ];
+            } else {
+                $tmp['forms']['products'] = $tmp['forms']['products'] . ",$formId";
             }
 
-            $this->formsIds[$formName] = $formId;
-            foreach ($this->formsIds as $formNameLocal => $formIdLocal) {
-                $tmp['forms'][$formNameLocal.'_id'] = $formIdLocal;
-            }
 
             $config->$baseKey = $tmp;
             $config->write();
@@ -92,83 +118,6 @@ class UpdateHandler__ extends YesWikiHandler
             unset($config);
             return $formId;
         }
-
         return null;
-    }
-
-    private function updateForm(string $formName): ?string
-    {
-        // get services
-        $formManager = $this->getService(FormManager::class);
-
-        // get file
-        if (!file_exists(self::PATHS['forms'][$formName])) {
-            return $this->render('@templates/alert-message.twig', [
-                'type' => 'danger',
-                'message' => str_replace(
-                    ['{formName}','{filePath}'],
-                    [$formName,self::PATHS['forms'][$formName]],
-                    _t('BENEVOLAT_UPDATE_FORM_ERROR')
-                ),
-            ]);
-        }
-        $formTemplate = file_get_contents(self::PATHS['forms'][$formName]);
-
-        // get Form Id
-        $formId = $this->getFormOrCreate($formName);
-        if (empty($formId)) {
-            return $this->render('@templates/alert-message.twig', [
-                'type' => 'danger',
-                'message' => str_replace(
-                    ['{formName}'],
-                    [$formName],
-                    _t('BENEVOLAT_UPDATE_FORM_ERROR_CREATION')
-                ),
-            ]);
-        }
-        // get linked form Id
-        $linkedFormName = $formName === 'Benevole' ? 'Benevolat' : 'Benevole';
-        $linkedFormId = $this->getFormOrCreate($linkedFormName);
-        if (empty($linkedFormId)) {
-            return $this->render('@templates/alert-message.twig', [
-                'type' => 'danger',
-                'message' => str_replace(
-                    ['{formName}'],
-                    [$linkedFormName],
-                    _t('BENEVOLAT_UPDATE_FORM_ERROR_CREATION')
-                ),
-            ]);
-        }
-
-        $creationProfilBenevoleUrl = $this->wiki->Href('', $this->pagesTags['CreationProfilBenevole']['new']);
-        $ajoutBenevolatUrl = $this->wiki->Href('', $this->pagesTags['AjoutBenevolat']['new']);
-        $suiviBenevolatUrl = $this->wiki->Href('', $this->pagesTags['SuiviBenevolat']['new']);
-        $listeMois = $this->listsIds['ListeMois']['new'];
-        $listeAnnees = $this->listsIds['ListeAnnees']['new'];
-
-        $formTemplate = str_replace(
-            ['{linkedFormId}','{creationProfilBenevoleUrl}','{ajoutBenevolatUrl}','{suiviBenevolatUrl}','{ListeMois}','{ListeAnnees}'],
-            [$linkedFormId,$creationProfilBenevoleUrl,$ajoutBenevolatUrl,$suiviBenevolatUrl,$listeMois,$listeAnnees],
-            $formTemplate
-        );
-
-        $formManager->update([
-            'bn_label_nature' => $formName,
-            'bn_template'=> $formTemplate,
-            'bn_description'=> '',
-            'bn_sem_context'=> '',
-            'bn_sem_type'=> '',
-            'bn_condition'=> '',
-            'bn_id_nature'=> $formId,
-        ]);
-
-        return $this->render('@templates/alert-message.twig', [
-            'type' => 'success',
-            'message' => str_replace(
-                ['{formName}'],
-                [$formName],
-                _t('BENEVOLAT_UPDATE_FORM_SUCCESS')
-            ),
-        ]);
     }
 }
