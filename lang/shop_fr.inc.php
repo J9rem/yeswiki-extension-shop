27b3a4e67816bc15b19725a9899a4dbf3d374623
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-12-09 20:20:34 +0100

    Revert "Merge remote-tracking branch 'eliot/main'"
    This reverts commit 05656aae4fb68988e6c862a9573c5432775d3bf2, reversing
    changes made to 12510307fce0b1106ea1dd552df7c3432d46a531.

--- lang/shop_fr.inc.php
+++ lang/shop_fr.inc.php
@@ -12,8 +12,6 @@
 return [
 
     // actions/EditConfigAction.php
-    'SHOP_UPDATE_FORM_ERROR' => "Impossible de mettre à jour le formulaire '{formName}' car le fichier '{filePath}' est introuvable.",
-    'EDIT_CONFIG_HINT_SHOP[FORMS][PRODUCTS]' => 'Identifiants de formulaires produits (séparés par des ,) - appeler /update pour mise à jour',
     'EDIT_CONFIG_GROUP_SHOP' => 'Extension "shop"',
     'EDIT_CONFIG_HINT_SHOP[SERVICENAME]' => 'Nom du système de paiement (helloasso) - EN TEST',
     'EDIT_CONFIG_HINT_SHOP[MANGOPAY][CLIENTID]' => 'Identifiant client MangoPay',
