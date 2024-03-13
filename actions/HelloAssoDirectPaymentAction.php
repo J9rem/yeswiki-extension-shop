068e4ee577127bf07e9db55504b0a9ec6d18a2b2
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2024-03-13 16:01:53 +0100

    feat(HelloAssoDirectPayment): link to template

--- actions/HelloAssoDirectPaymentAction.php
+++ actions/HelloAssoDirectPaymentAction.php
@@ -13,12 +13,33 @@
 namespace YesWiki\Shop;
 
 use YesWiki\Core\YesWikiAction;
+use YesWiki\Shop\Entity\HelloAssoDirectPaymentData;
+use YesWiki\Shop\Service\HelloAssoDirectPaymentService;
 
 class HelloAssoDirectPaymentAction extends YesWikiAction
 {
     public function formatArguments($arg)
     {
         $newArgs = [];
+        foreach([
+            'totalAmount',
+            'itemName',
+            'shop backUrl',
+            'shop errorUrl',
+            'shop returnUrl',
+            'firstName',
+            'lastName',
+            'email',
+            'birthDate',
+            'address',
+            'city',
+            'zipCode',
+            'country',
+            'meta',
+        ] as $key) {
+            $newArgs[$key] = $this->formatString($arg, $key);
+        }
+        $newArgs['containsDonation'] = $this->formatBoolean($arg, false, 'containsDonation');
         return $newArgs;
     }
 
@@ -31,9 +52,26 @@ class HelloAssoDirectPaymentAction extends YesWikiAction
 
     public function run()
     {
-        return $this->render('@templates/alert-message.twig', [
-            'type' => 'warning',
-            'message' => 'Action en cours de conception'
+        $data = new HelloAssoDirectPaymentData(
+            $this->arguments['totalAmount'],
+            $this->arguments['itemName'],
+            $this->arguments['shop backUrl'],
+            $this->arguments['shop errorUrl'],
+            $this->arguments['shop returnUrl'],
+            $this->arguments['containsDonation'],
+            $this->arguments['firstName'],
+            $this->arguments['lastName'],
+            $this->arguments['email'],
+            $this->arguments['birthDate'],
+            $this->arguments['address'],
+            $this->arguments['city'],
+            $this->arguments['zipCode'],
+            $this->arguments['country'],
+            $this->arguments['meta']
+        );
+        return $this->render('@shop/hello-asso-direct-payment-action.twig', [
+            'args' => $this->arguments,
+            'token' => $this->getService(HelloAssoDirectPaymentService::class)->getUpdatedToken($data)
         ]);
     }
 }
